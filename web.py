56e358be4e53e5ca97d2e76d7961448ce8c39c28
i5-PC <mcienkowski@gmail.com>
2023-08-20 15:19:33 +0200

    Changed the input place

--- web.py
+++ web.py
@@ -13,6 +13,8 @@ st.title("My Todo App")
 st.write("This <b>app</b> is to increase your <b>productivity.</b>",
          unsafe_allow_html=True)
 
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -22,5 +24,3 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="", placeholder="Add new todo...",
-              on_change=add_todo, key='new_todo')
